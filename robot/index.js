d1e985742430ffc2a0e07339a7c5ed96cd0af9e8
jiangfeng <jiangfeng_130122@163.com>
2017-05-16 20:29:37 +0800

    更新

--- robot/index.js
+++ robot/index.js
@@ -9,7 +9,7 @@ var loginPage = require('./pages/login')
 var complaints = require('./pages/complaints')
 // 异常订单
 var exceptionOrder = require('./pages/exception-order')
-
+console.log(nightmare)
 var nightmare = Nightmare(config.nightmare)
   .on('did-finish-load', function () {
     console.log('did-finish-load')

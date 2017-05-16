014a442de1eeef696d268d8b32cd85978234ab7f
jiangfeng <jiangfeng_130122@163.com>
2017-05-16 19:59:18 +0800

    更新配置文件，更新环境变量

--- runtime/App/AppConfig.js
+++ runtime/App/AppConfig.js
@@ -1,5 +1,5 @@
 let deepAssign = require('deep-assign')
-let baseConfig = require('../../app.config.json')
-let AppConfig = require('../../app.config' + (process.env.ENV ? ('.' + process.env.ENV) : '.development'))
+let baseConfig = require('../../app.config')
+let AppConfig = require('../../app.config' + (process.env.NODE_ENV ? ('.' + process.env.NODE_ENV) : '.development'))
 let config = deepAssign({}, baseConfig, AppConfig)
 module.exports = config

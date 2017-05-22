37480c918a28d10afa5a97d34262de6875ed0898
jiangfeng <jiangfeng_130122@163.com>
2017-05-22 13:46:39 +0800

    更新异常订单统计数的代码结构

--- robot/pages/exception-order.js
+++ robot/pages/exception-order.js
@@ -30,9 +30,6 @@ class ExceptionOrder {
       .get()
       .then(function (cookies) {
         that.init(cookies)
-          .then(function () {
-            that.monitor()
-          })
       })
   }
   init (cookies) {
@@ -40,52 +37,62 @@ class ExceptionOrder {
     if (!that.nightmare) {
       that.nightmare = new Nightmare(config.nightmare)
         .on('console', function (type, msg) {
-          console[type]('evaluate :' + msg)
+          console[type]('evaluate log :' + msg)
         })
-      that.nightmare
+        .on('did-finish-load', function () {
+          that.nightmare
+            .url()
+            .then(function (url) {
+              log.info('//--------------------【异常订单统计数监控】URL---------------//')
+              log.info(url)
+              if (url.indexOf('php/index.php?d=seller&c=seller&m=getAbnormalDealList&dealid=&state=3&time_begin=&time_end=&dealType=0') > -1) {
+                that.monitor()
+              } else if (url.indexOf('php/index.php?d=seller&c=sellerLogin&m=login') > -1) {
+                log.warn('//--------------------【异常订单统计数监控】用户过期，需要重新登录----------------//')
+                that.eventEmitter.emit('login-expired', that)
+              }
+            })
+        })
         .goto('http://chong.qq.com/')
         .cookies
         .set(cookies)
+        .goto('http://chong.qq.com/php/index.php?d=seller&c=seller&m=getAbnormalDealList&dealid=&state=3&time_begin=&time_end=&dealType=0&r=' + new Date() * 1)
+        .run(function () {
+          log.info('已进入异常订单统计数监听页面')
+          that.timeTick()
+        })
     }
-    return that.nightmare
   }
-  monitor () {
+  timeTick () {
     var that = this
     setInterval(function () {
       that.nightmare
         .goto('http://chong.qq.com/php/index.php?d=seller&c=seller&m=getAbnormalDealList&dealid=&state=3&time_begin=&time_end=&dealType=0&r=' + new Date() * 1)
-        .url()
-        .then(function (url) {
-          log.info('初始化异常订单监听窗口')
-          console.log(url)
-          if (url.indexOf('http://chong.qq.com/php/index.php?d=seller&c=sellerLogin&m=login') > -1) {
-            log.warn('//--------------------【异常订单统计数量监控】用户过期，需要重新登录----------------//')
-            return that.nightmare.end().then(function () {
-              that.nightmare = null
-              that.eventEmitter.emit('login-expired', that)
-            })
-          }
-        })
-        .then(function () {
-          that.nightmare.wait('.ui-page-cont')
-            .evaluate(function () {
-              var pager = document.querySelector('.ui-page-cont')
-              var lastPage = pager.lastElementChild
-              console.log(lastPage.innerText)
-              if (lastPage.innerText === '尾页') {
-                return 'http://chong.qq.com' + lastPage.getAttribute('href')
-              } else {
-                return ''
-              }
-            })
-            .then(function (url) {
-              log.info(url)
-              if (!url) return
-              var countTotal = +_queryString('page', url)
-              service.pushExceptionOrders(countTotal * 20)
-            })
+        .run(function () {
+          log.info('已刷新异常订单统计数监听页面')
         })
     }, config.worker.tickTime)
+  }
+  monitor () {
+    var that = this
+    that.nightmare
+      .wait('.ui-page-cont')
+      .evaluate(function () {
+        var pager = document.querySelector('.ui-page-cont')
+        var lastPage = pager.lastElementChild
+        console.log(lastPage.innerText)
+        if (lastPage.innerText === '尾页') {
+          return 'http://chong.qq.com' + lastPage.getAttribute('href')
+        } else {
+          return ''
+        }
+      })
+      .then(function (url) {
+        log.info(url)
+        if (!url) return
+        var countTotal = +_queryString('page', url)
+        service.pushExceptionOrders(countTotal * 20)
+      })
   }
 }
 
